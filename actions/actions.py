ad9ce78d21fec283affc7afd64261f7592dd02ef
kuluru vineeth kumar reddy <kuluruvineeth8623@gmail.com>
2021-03-30 17:08:53 +0530

    updated

--- actions/actions.py
+++ actions/actions.py
@@ -26,13 +26,63 @@
 #
 #         return []
 
-from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List,Union
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+from rasa_sdk.events import EventType
+from utils.fertilizer import fertilizer_dic
+import pickle
+import pandas as pd
+import numpy as np
 
 import requests
+
+from excel_data import DataStore
+
+crop_recommendation_model_path = 'RandomForest.pkl'
+crop_recommendation_model = pickle.load(open(crop_recommendation_model_path, 'rb'))
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = 'b8e63e97d870d37437a1bf6a70f3de3f'
+    current=requests.get("http://api.openweathermap.org/data/2.5/weather?appid={}&q={}".format(api_key,city_name))
+
+    temperature=current.json()['main']['humidity']
+    humidity=current.json()['main']['temp']
+
+
+
+
+
+    temperature = round((temperature - 273.15), 2)
+    return temperature, humidity
+
+
+
+class ActionSaveData(Action):
+    def name(self) -> Text:
+        return "action_save_data"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        DataStore(tracker.get_slot("nitrogen"),
+            tracker.get_slot("phosphorous"),
+            tracker.get_slot("potassium"),
+            tracker.get_slot("crop"))
+        dispatcher.utter_message(text="Data noted")
+
+        return []
+
+
+
 
 
 class ActionCheckPrice(Action):
@@ -42,6 +92,7 @@ class ActionCheckPrice(Action):
 
     def run(self, dispatcher, tracker, domain):
         crop = tracker.get_slot('crop')
+        print(crop)
         api_key = '579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b'
         loc = tracker.get_slot('location')
         
@@ -57,7 +108,10 @@ class ActionCheckPrice(Action):
             yr2=current.json()['field'][2]['name']
             yr3=current.json()['field'][3]['name']
 
-            response = """The msp for {} in the year {} was {}  \n The msp of {} in the year {} was {} \n The msp for {} in the year {} was {}  """.format(crop,yr1, msp_1,crop, yr2,msp_2,crop, yr3,msp_3)
+            
+            response = """Crop |\t\t\t Year \t\t\t\t\t |MSP\t\t\t|\nRice |\t\t\t {} \t\t\t\t\t |{}\t\t\t|\nRice |\t\t\t {} \t\t\t\t\t |{}\t\t\t|\nRice |\t\t\t {} \t\t\t\t\t |\t\t\t{}""".format(yr1,msp_1,yr2,msp_2,yr3,msp_3)
+
+
             dispatcher.utter_message(text=response)
             return []
 
@@ -132,6 +186,148 @@ class ActionFertilizerConsumption(Action):
         dispatcher.utter_message(text=response)
 
         return []
+
+
+class FertilizerRecommendation(FormAction):
+    def name(self) -> Text:
+        return "fertilizer_info"
+    @staticmethod
+    def required_slots(tracker: "Tracker") -> List[Text]:
+        return ["nitrogen","phosphorous","potassium","crop"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+        return {
+        "nitrogen":[self.from_text()],
+        "phosphorous":[self.from_text()],
+        "potassium":[self.from_text()],
+        "crop":[self.from_text()]
+        }
+
+    def submit(
+        self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+
+        dispatcher.utter_message("Here are the information that you provided.\nNitrogen: {0},\nPhosphorous: {1},\nPotassium: {2},\ncrop: {3}".format(
+        tracker.get_slot("nitrogen"),
+        tracker.get_slot("phosphorous"),
+        tracker.get_slot("potassium"),
+        tracker.get_slot("crop")
+        ))
+        return []
+
+class ActionFertilizerRecommendation(Action):
+    def name(self) -> Text:
+        return "action_ferti_recommendation"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        crop_name = tracker.get_slot('crop')
+        N = tracker.get_slot('nitrogen')
+        P = tracker.get_slot('phosphorous')
+        K = tracker.get_slot('potassium')
+        print(crop_name)
+        N1 = N.split("_", 1)
+        print(int(N1[1]))
+        P1 = P.split("_", 1)
+        print(int(P1[1]))
+        K1 = K.split("_", 1)
+        print(int(K1[1]))
+        # ph = float(request.form['ph'])
+
+        df = pd.read_csv('fertilizer.csv')
+
+        nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+        pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+        kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+
+        n = nr - int(N1[1])
+        p = pr - int(P1[1])
+        k = kr - int(K1[1])
+        temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
+        max_value = temp[max(temp.keys())]
+        if max_value == "N":
+            if n < 0:
+                key = 'NHigh'
+            else:
+                key = "Nlow"
+        elif max_value == "P":
+            if p < 0:
+                key = 'PHigh'
+            else:
+                key = "Plow"
+        else:
+            if k < 0:
+                key = 'KHigh'
+            else:
+                key = "Klow"
+
+        response = (fertilizer_dic[key])
+        dispatcher.utter_message(text=response)
+
+        
+
+        return []
+
+
+class ActionFertilizerRecommendation(Action):
+    def name(self) -> Text:
+        return "action_crop_recommendation"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        crop_name = tracker.get_slot('crop')
+        N = tracker.get_slot('nitrogen')
+        P = tracker.get_slot('phosphorous')
+        K = tracker.get_slot('potassium')
+        print(crop_name)
+        N1 = N.split("_", 1)
+        print(int(N1[1]))
+        P1 = P.split("_", 1)
+        print(int(P1[1]))
+        K1 = K.split("_", 1)
+        print(int(K1[1]))
+        # ph = float(request.form['ph'])
+
+        ph = tracker.get_slot('ph')
+        rainfall = tracker.get_slot('rainfall')
+        ph1 = ph.split("_", 1)
+        print(float(ph1[1]))
+        rainfall1 = rainfall.split("_", 1)
+        print(float(rainfall1[1]))
+
+
+        city = tracker.get_slot("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            print(temperature)
+            print(humidity)
+            data = np.array([[int(N1[1]), int(P1[1]), int(K1[1]), temperature, humidity, float(ph1[1]), float(rainfall1[1])]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+            print(final_prediction)
+
+
+
+        response = """The crop recommended to grow according to our prediction is {}""".format(final_prediction)
+
+
+        
+        dispatcher.utter_message(text=response)
+
+        
+
+        return []
+
+
+
 
 
 
